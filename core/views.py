004f3547e320e0b7e41e7c415a43faa16ac3c58d
Samuel Birhanu <samibre121@gmail.com>
2024-08-08 00:28:24 +0300

    added a deafault router and created a viewSet for the Exchange model

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Exchange
+from .serializers import ExchangeSerializer
 
-# Create your views here.
+class ExchangeViewSet(viewsets.ModelViewSet):
+    queryset = Exchange.objects.all()
+    serializer_class = ExchangeSerializer
